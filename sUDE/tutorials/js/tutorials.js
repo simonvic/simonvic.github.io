f02b13ef78e5cb75b6eac2d875f2168a333fefce
simonvic <simonvic.dev@gmail.com>
2025-11-09 00:25:05 +0100

    fix(sUDE): aria-busy causing :target anchors to shift up

--- sUDE/tutorials/js/tutorials.js
+++ sUDE/tutorials/js/tutorials.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+	document.getElementsByTagName("nav")[0].setAttribute("aria-busy", "true");
 	var split = document.location.href.split("/");
 	var id = split[split.length - 2];
 	fetchXML("/sUDE/tutorials/tutorials.xml", xml => onTutorialLoad(parseTutorialCard(xml.getElementById(id))));
@@ -7,5 +8,5 @@ window.onload = () => {
 function onTutorialLoad(tutorial) {
 	document.title = `sUDE | ${tutorial.title}`;
 	document.getElementById("breadcrumb_current").innerHTML = tutorial.title;
-	document.getElementsByTagName("main")[0].setAttribute("aria-busy", "false");
+	document.getElementsByTagName("nav")[0].setAttribute("aria-busy", "false");
 }
