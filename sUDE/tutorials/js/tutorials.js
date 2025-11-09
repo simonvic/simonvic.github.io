b1b746d76d4ce5dbed0c5a6f88fc06906f99bb7e
simonvic <simonvic.dev@gmail.com>
2025-11-09 02:03:45 +0100

    feat(sUDE): highlight observable section in ToC

--- sUDE/tutorials/js/tutorials.js
+++ sUDE/tutorials/js/tutorials.js
@@ -1,5 +1,19 @@
 window.onload = () => {
 	document.getElementsByTagName("nav")[0].setAttribute("aria-busy", "true");
+
+	const toc = document.querySelector("main aside nav")
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const a = toc.querySelector(`a[href="#${entry.target.id}"]`)
+			if (entry.isIntersecting) {
+				a.classList.add("active")
+			} else {
+				a.classList.remove("active")
+			}
+		});
+	});
+	document.querySelectorAll("section[id]").forEach(section => observer.observe(section));
+
 	var split = document.location.href.split("/");
 	var id = split[split.length - 2];
 	fetchXML("/sUDE/tutorials/tutorials.xml", xml => onTutorialLoad(parseTutorialCard(xml.getElementById(id))));
